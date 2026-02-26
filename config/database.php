2b911b3b7421d983687e0e595bee1eb84db1a44b
hamzaelk04 <hamzakabraoui07@gmail.com>
2026-02-26 22:04:00 +0000

    creating a new migration references on a pivot table between users and colocations named user_colocation

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => 'Innodb',
+            'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
---
+++ database/migrations/2026_02_26_215232_create_colocation_user_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_colocations', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('colocation_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('colocation_id')->references('id')->on('colocations');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_colocations');
+    }
+};
